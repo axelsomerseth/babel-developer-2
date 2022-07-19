441e4ce3f54dec14e99358513dba6f060c8a35e6
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-07-19 08:56:30 -0600

    preset added

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 const plugins = ["babel-plugin-transform-async-to-promises"];
+
 const targets = {
   chrome: "61",
 };
 
-module.exports = { plugins, targets };
+// Uncomment to use preset instead of plug-ins.
+// const presets = ["@babel/preset-env"];
+
+module.exports = {
+  plugins,
+  targets,
+  // presets,
+};
